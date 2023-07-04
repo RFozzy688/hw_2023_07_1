508ee17d12477019d2b8c7ffd6a49c9be9daa561
RFozzy688 <pokerfozzy@gmail.com>
2023-07-04 15:41:12 +0300

    add localizable

--- hw_2023.07.1/Form1.Designer.cs
+++ hw_2023.07.1/Form1.Designer.cs
@@ -28,52 +28,144 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             button1 = new Button();
             button2 = new Button();
             button3 = new Button();
+            menuStrip1 = new MenuStrip();
+            fileToolStripMenuItem = new ToolStripMenuItem();
+            createToolStripMenuItem = new ToolStripMenuItem();
+            openToolStripMenuItem = new ToolStripMenuItem();
+            closeToolStripMenuItem = new ToolStripMenuItem();
+            editToolStripMenuItem = new ToolStripMenuItem();
+            cutToolStripMenuItem = new ToolStripMenuItem();
+            copyToolStripMenuItem = new ToolStripMenuItem();
+            pastToolStripMenuItem = new ToolStripMenuItem();
+            textBox1 = new TextBox();
+            label1 = new Label();
+            textBox2 = new TextBox();
+            label2 = new Label();
+            button4 = new Button();
+            menuStrip1.SuspendLayout();
             SuspendLayout();
             // 
             // button1
             // 
+            resources.ApplyResources(button1, "button1");
             button1.BackgroundImage = Properties.Resources.usa;
-            button1.BackgroundImageLayout = ImageLayout.Stretch;
-            button1.Location = new Point(376, 12);
             button1.Name = "button1";
-            button1.Size = new Size(99, 44);
-            button1.TabIndex = 0;
             button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
             // 
             // button2
             // 
+            resources.ApplyResources(button2, "button2");
             button2.BackgroundImage = Properties.Resources.ua;
-            button2.BackgroundImageLayout = ImageLayout.Stretch;
-            button2.Location = new Point(376, 62);
             button2.Name = "button2";
-            button2.Size = new Size(99, 44);
-            button2.TabIndex = 1;
             button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
             // 
             // button3
             // 
+            resources.ApplyResources(button3, "button3");
             button3.BackgroundImage = Properties.Resources.pl;
-            button3.BackgroundImageLayout = ImageLayout.Stretch;
-            button3.Location = new Point(376, 112);
             button3.Name = "button3";
-            button3.Size = new Size(99, 44);
-            button3.TabIndex = 2;
             button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // menuStrip1
+            // 
+            resources.ApplyResources(menuStrip1, "menuStrip1");
+            menuStrip1.Items.AddRange(new ToolStripItem[] { fileToolStripMenuItem, editToolStripMenuItem });
+            menuStrip1.Name = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            resources.ApplyResources(fileToolStripMenuItem, "fileToolStripMenuItem");
+            fileToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { createToolStripMenuItem, openToolStripMenuItem, closeToolStripMenuItem });
+            fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            // 
+            // createToolStripMenuItem
+            // 
+            resources.ApplyResources(createToolStripMenuItem, "createToolStripMenuItem");
+            createToolStripMenuItem.Name = "createToolStripMenuItem";
+            // 
+            // openToolStripMenuItem
+            // 
+            resources.ApplyResources(openToolStripMenuItem, "openToolStripMenuItem");
+            openToolStripMenuItem.Name = "openToolStripMenuItem";
+            // 
+            // closeToolStripMenuItem
+            // 
+            resources.ApplyResources(closeToolStripMenuItem, "closeToolStripMenuItem");
+            closeToolStripMenuItem.Name = "closeToolStripMenuItem";
+            // 
+            // editToolStripMenuItem
+            // 
+            resources.ApplyResources(editToolStripMenuItem, "editToolStripMenuItem");
+            editToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { cutToolStripMenuItem, copyToolStripMenuItem, pastToolStripMenuItem });
+            editToolStripMenuItem.Name = "editToolStripMenuItem";
+            // 
+            // cutToolStripMenuItem
+            // 
+            resources.ApplyResources(cutToolStripMenuItem, "cutToolStripMenuItem");
+            cutToolStripMenuItem.Name = "cutToolStripMenuItem";
+            // 
+            // copyToolStripMenuItem
+            // 
+            resources.ApplyResources(copyToolStripMenuItem, "copyToolStripMenuItem");
+            copyToolStripMenuItem.Name = "copyToolStripMenuItem";
+            // 
+            // pastToolStripMenuItem
+            // 
+            resources.ApplyResources(pastToolStripMenuItem, "pastToolStripMenuItem");
+            pastToolStripMenuItem.Name = "pastToolStripMenuItem";
+            // 
+            // textBox1
+            // 
+            resources.ApplyResources(textBox1, "textBox1");
+            textBox1.Name = "textBox1";
+            // 
+            // label1
+            // 
+            resources.ApplyResources(label1, "label1");
+            label1.Name = "label1";
+            // 
+            // textBox2
+            // 
+            resources.ApplyResources(textBox2, "textBox2");
+            textBox2.Name = "textBox2";
+            // 
+            // label2
+            // 
+            resources.ApplyResources(label2, "label2");
+            label2.Name = "label2";
+            // 
+            // button4
+            // 
+            resources.ApplyResources(button4, "button4");
+            button4.Name = "button4";
+            button4.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            resources.ApplyResources(this, "$this");
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(487, 450);
+            Controls.Add(button4);
+            Controls.Add(label2);
+            Controls.Add(textBox2);
+            Controls.Add(label1);
+            Controls.Add(textBox1);
             Controls.Add(button3);
             Controls.Add(button2);
             Controls.Add(button1);
+            Controls.Add(menuStrip1);
+            MainMenuStrip = menuStrip1;
             Name = "Form1";
-            Text = "Form1";
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
@@ -81,5 +173,19 @@
         private Button button1;
         private Button button2;
         private Button button3;
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem fileToolStripMenuItem;
+        private ToolStripMenuItem createToolStripMenuItem;
+        private ToolStripMenuItem openToolStripMenuItem;
+        private ToolStripMenuItem closeToolStripMenuItem;
+        private ToolStripMenuItem editToolStripMenuItem;
+        private ToolStripMenuItem cutToolStripMenuItem;
+        private ToolStripMenuItem copyToolStripMenuItem;
+        private ToolStripMenuItem pastToolStripMenuItem;
+        private TextBox textBox1;
+        private Label label1;
+        private TextBox textBox2;
+        private Label label2;
+        private Button button4;
     }
 }
--- hw_2023.07.1/Form1.cs
+++ hw_2023.07.1/Form1.cs
@@ -1,9 +1,32 @@
+using System.Globalization;
+
 namespace hw_2023._07._1
 {
     public partial class Form1 : Form
     {
         public Form1()
         {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Thread.CurrentThread.CurrentUICulture = CultureInfo.GetCultureInfo("uk-UA");
+            Controls.Clear();
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Thread.CurrentThread.CurrentUICulture = CultureInfo.GetCultureInfo("en-US");
+            Controls.Clear();
+            InitializeComponent();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Thread.CurrentThread.CurrentUICulture = CultureInfo.GetCultureInfo("pl-PL");
+            Controls.Clear();
             InitializeComponent();
         }
     }
